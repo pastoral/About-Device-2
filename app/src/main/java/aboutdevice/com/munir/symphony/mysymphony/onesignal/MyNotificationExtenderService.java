5ec801cec39b540129c33b3db4f864df5f6c629b
pastoral <munir.hoque@gmail.com>
2017-01-17 16:51:28 +0600

    SW version wise notification classification

--- app/src/main/java/aboutdevice/com/munir/symphony/mysymphony/onesignal/MyNotificationExtenderService.java
+++ app/src/main/java/aboutdevice/com/munir/symphony/mysymphony/onesignal/MyNotificationExtenderService.java
@@ -2,15 +2,20 @@ package aboutdevice.com.munir.symphony.mysymphony.onesignal;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.onesignal.NotificationExtenderService;
 import com.onesignal.OSNotificationDisplayedResult;
 import com.onesignal.OSNotificationReceivedResult;
 
+import org.json.JSONObject;
+
 import java.math.BigInteger;
 
+import aboutdevice.com.munir.symphony.mysymphony.MainActivity;
 import aboutdevice.com.munir.symphony.mysymphony.MySymphonyApp;
 import aboutdevice.com.munir.symphony.mysymphony.R;
 
@@ -21,21 +26,58 @@ import aboutdevice.com.munir.symphony.mysymphony.R;
 public class MyNotificationExtenderService  extends NotificationExtenderService{
     @Override
     protected boolean onNotificationProcessing(OSNotificationReceivedResult notification) {
+        JSONObject data = notification.payload.additionalData;
+        String modelSWVersion;
+        boolean returnVal;
+        modelSWVersion = data.optString("modelSWVersion", null);
+
         OverrideSettings overrideSettings = new OverrideSettings();
-        overrideSettings.extender = new NotificationCompat.Extender() {
-            @Override
-            public NotificationCompat.Builder extend(NotificationCompat.Builder builder) {
-                // Sets the background notification color to Red on Android 5.0+ devices.
-                Bitmap icon = BitmapFactory.decodeResource(MySymphonyApp.getContext().getResources(),
-                        R.drawable.smiley);
-                builder.setLargeIcon(icon);
-                return builder.setColor(new BigInteger("FF0000FF", 16).intValue());
-            }
-        };
 
-        OSNotificationDisplayedResult displayedResult = displayNotification(overrideSettings);
-        Log.d("OneSignalExample", "Notification displayed with id: " + displayedResult.androidNotificationId);
+        if (modelSWVersion != null && modelSWVersion.equals(getSystemProperty("ro.custom.build.version"))) {
+            overrideSettings.extender = new NotificationCompat.Extender() {
+                @Override
+                public NotificationCompat.Builder extend(NotificationCompat.Builder builder) {
+                    // Sets the background notification color to Red on Android 5.0+ devices.
+                    Bitmap icon = BitmapFactory.decodeResource(MySymphonyApp.getContext().getResources(),
+                            R.drawable.smiley);
+                    builder.setLargeIcon(icon);
+                    return builder.setColor(new BigInteger("FF0000FF", 16).intValue());
+                }
+            };
 
+            OSNotificationDisplayedResult displayedResult = displayNotification(overrideSettings);
+            Log.d("OneSignalExample", "Notification displayed with id: " + displayedResult.androidNotificationId);
+
+        }
+
+        else if(modelSWVersion != null && modelSWVersion.equals("any")){
+            overrideSettings.extender = new NotificationCompat.Extender() {
+                @Override
+                public NotificationCompat.Builder extend(NotificationCompat.Builder builder) {
+                    // Sets the background notification color to Red on Android 5.0+ devices.
+                    Bitmap icon = BitmapFactory.decodeResource(MySymphonyApp.getContext().getResources(),
+                            R.drawable.smiley);
+                    builder.setLargeIcon(icon);
+                    return builder.setColor(new BigInteger("FF0000FF", 16).intValue());
+                }
+            };
+
+            OSNotificationDisplayedResult displayedResult = displayNotification(overrideSettings);
+            Log.d("OneSignalExample", "Notification displayed with id: " + displayedResult.androidNotificationId);
+        }
         return true;
+    }
+
+    public String getSystemProperty(String key) {
+        String value = null;
+
+        try {
+            value = (String) Class.forName("android.os.SystemProperties")
+                    .getMethod("get", String.class).invoke(null, key);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return value;
     }
 }
