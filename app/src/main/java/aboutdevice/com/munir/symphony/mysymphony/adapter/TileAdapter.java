8285b14e266abf1ecf91ee9b6355cf76d5a87f46
pastoral <munir.hoque@gmail.com>
2017-01-31 16:42:16 +0600

    modify the UI of Contact activity

--- app/src/main/java/aboutdevice/com/munir/symphony/mysymphony/adapter/TileAdapter.java
+++ app/src/main/java/aboutdevice/com/munir/symphony/mysymphony/adapter/TileAdapter.java
@@ -1,12 +1,15 @@
 package aboutdevice.com.munir.symphony.mysymphony.adapter;
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Random;
 
 import aboutdevice.com.munir.symphony.mysymphony.R;
 
@@ -16,6 +19,7 @@ import aboutdevice.com.munir.symphony.mysymphony.R;
 
 public class TileAdapter extends RecyclerView.Adapter<TileAdapter.TileViewHolder> {
     private Context context;
+    private Random mRandom = new Random();
     int[] imgList = {R.drawable.toolbar, R.drawable.features, R.drawable.customer_care};
     String[] nameList = {"Hotline", "Features", "Coustomer Care"};
     public TileAdapter(Context context){
@@ -24,10 +28,13 @@ public class TileAdapter extends RecyclerView.Adapter<TileAdapter.TileViewHolder
     public class TileViewHolder extends RecyclerView.ViewHolder{
         ImageView imageView;
         TextView textView;
+        CardView cardView;
         public TileViewHolder(View view){
             super(view);
             imageView = (ImageView)view.findViewById(R.id.tileImage);
             textView = (TextView)view.findViewById(R.id.tileName);
+            cardView = (CardView)view.findViewById(R.id.card_view);
+
         }
     }
 
@@ -41,6 +48,7 @@ public class TileAdapter extends RecyclerView.Adapter<TileAdapter.TileViewHolder
 
     @Override
     public void onBindViewHolder(TileViewHolder holder, int position) {
+        holder.cardView.getLayoutParams().height = getRandomIntInRange(290,125);
         holder.imageView.setImageResource(imgList[position]);
         holder.textView.setText(nameList[position]);
     }
@@ -49,4 +57,9 @@ public class TileAdapter extends RecyclerView.Adapter<TileAdapter.TileViewHolder
     public int getItemCount() {
         return nameList.length;
     }
+
+    protected int getRandomIntInRange(int max, int min){
+        return mRandom.nextInt((max-min)+min)+min;
+    }
+
 }
--- app/src/main/java/aboutdevice/com/munir/symphony/mysymphony/ui/FourFrgment.java
+++ app/src/main/java/aboutdevice/com/munir/symphony/mysymphony/ui/FourFrgment.java
@@ -4,19 +4,30 @@ import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
 
 import java.util.List;
 
 import aboutdevice.com.munir.symphony.mysymphony.MySymphonyApp;
 import aboutdevice.com.munir.symphony.mysymphony.R;
+import aboutdevice.com.munir.symphony.mysymphony.adapter.TileAdapter;
 
 /**
  * Created by munirul.hoque on 11/15/2016.
@@ -24,28 +35,52 @@ import aboutdevice.com.munir.symphony.mysymphony.R;
 
 public class FourFrgment extends Fragment {
     public LinearLayout contactline1, contactline2, facebookarea;
+    public ImageView icon1, icon2, icon3, icon4;
+    public CardView card1, card2, card3, card4;
+    public RecyclerView recyclerView;
+    private View view;
+    private RecyclerView.Adapter mAdapter;
+    StaggeredGridLayoutManager layoutManager;
     public FourFrgment() {
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_four,container,false);
-        contactline1 = (LinearLayout)view.findViewById(R.id.contactline1);
+         view = inflater.inflate(R.layout.fragment_four,container,false);
+       /* contactline1 = (LinearLayout)view.findViewById(R.id.contactline1);
         contactline2 = (LinearLayout)view.findViewById(R.id.contactline2);
-        facebookarea = (LinearLayout)view.findViewById(R.id.facebookarea);
+        facebookarea = (LinearLayout)view.findViewById(R.id.facebookarea); */
+      //  recyclerView = (RecyclerView)view.findViewById(R.id.contact_recycler);
+        icon1 = (ImageView)view.findViewById(R.id.icon1);
+        icon2 = (ImageView)view.findViewById(R.id.icon2);
+        icon3 = (ImageView)view.findViewById(R.id.icon3);
+        icon4 = (ImageView)view.findViewById(R.id.icon4);
+
+        card1 = (CardView)view.findViewById(R.id.card1);
+        card2 = (CardView)view.findViewById(R.id.card2);
+        card3 = (CardView)view.findViewById(R.id.card3);
+        card4 = (CardView)view.findViewById(R.id.card4);
         return view;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+       // layoutManager = new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
+        //layoutManager.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        contactline1.setOnClickListener(new View.OnClickListener() {
+        icon1.setImageDrawable(drawIcon(alphbetSelect("Call")));
+        icon2.setImageDrawable(drawIcon(alphbetSelect("Call")));
+        icon3.setImageDrawable(drawIcon(alphbetSelect("Facebook")));
+        icon4.setImageDrawable(drawIcon(alphbetSelect("WWW")));
+
+        card1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Intent.ACTION_DIAL);
@@ -54,7 +89,7 @@ public class FourFrgment extends Fragment {
             }
         });
 
-        contactline2.setOnClickListener(new View.OnClickListener() {
+        card2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Intent.ACTION_DIAL);
@@ -63,10 +98,20 @@ public class FourFrgment extends Fragment {
             }
         });
 
-        facebookarea.setOnClickListener(new View.OnClickListener() {
+        card3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 launchFacebook();
+            }
+        });
+
+        card4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://www.symphony-mobile.com";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
             }
         });
 
@@ -90,4 +135,37 @@ public class FourFrgment extends Fragment {
 
         startActivity(intent);
     }
+
+    public TextDrawable drawIcon(String str)
+    {
+        ColorGenerator generator = ColorGenerator.MATERIAL;
+        int color = generator.getRandomColor();
+        TextDrawable textDrawable = TextDrawable.builder()
+                .beginConfig()
+                .textColor(Color.WHITE)
+                .useFont(Typeface.DEFAULT)
+                .fontSize(40) /* size in px */
+                .bold()
+                .toUpperCase()
+                .endConfig()
+                .buildRound(str, color);
+        return textDrawable;
+    }
+
+    public String alphbetSelect(String str)
+    {
+        String firstAlphabet;
+        String ccName = str.trim();
+        if(ccName != null)
+        {
+            firstAlphabet = ccName.substring(0,1);
+        }
+        else
+        {
+            firstAlphabet = "A";
+        }
+
+        return  firstAlphabet;
+    }
+
 }
